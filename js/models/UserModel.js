d6df82d8ad0b7cb2c69286988361c6ec89149d93
WNRPTR <pedrique2002@gmail.com>
2021-05-26 18:46:47 +0100

    melhoramentos

--- js/models/UserModel.js
+++ js/models/UserModel.js
@@ -1,7 +1,8 @@
 export default class UserModel {
-    constructor(id, username, password) {
+    constructor(id, username, password, type) {
         this.id = id
         this.username = username
         this.password = password
+        this.type = type
     }
 }
