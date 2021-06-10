ad752b1fc693759a26a2ddfa5931348c7a1c47ef
Joao Santoe <joaosantos058@gmail.comm>
2021-06-10 16:23:24 +0100

    f

--- js/views/InicialPageView.js
+++ js/views/InicialPageView.js
@@ -3,7 +3,8 @@ import CategoryController from '../controllers/CategoryController.js'
 
 export default class InicialPageView {
     constructor() {
-
+        this.userController = new UserController();
+        this.categoryController = new CategoryController();
 
 
     }
